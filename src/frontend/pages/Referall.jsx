f1c6670d5feeae1927ed7374fe170677834aa7f6
Asa Rogers <asa.rogers@sonoco.com>
2022-10-09 08:21:45 -0500

    updates

--- src/frontend/pages/Referall.jsx
+++ src/frontend/pages/Referall.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import { useState } from "react";
 //main function for the referral
 import image from "../components/images/Bgimage.jpeg";
+import emailjs from "emailjs-com";
+
 function Referral() {
+  const [referredEmail, setReferredEmail] = useState("");
+  
+
+  function notifyEmployee(e) {
+    e.preventDefault();
+    emailjs.send(
+      "service_i6axn1p",
+      "template_skyzdns",
+      {
+        to_name: referredEmail,
+      },
+      "Moa0t217dvVU8rmk6"
+    );
+  }
+
   return (
     <div>
-      <div class="development-end"> </div>
-
       <div class="Body-background" style={{ backgroundImage: `url(${image})` }}>
         <div class="development-end"> </div>
         <div class="referral-background">
@@ -15,6 +31,16 @@ function Referral() {
             <div class="progress-bar-fill"> </div>
           </div>
         </div>
+        <input
+          type={"email"}
+          placeholder="Refer a friend"
+          className="referral-input"
+          //onChange passes the entire tag as the variable e, reads target is the elements inside the tag, and value is the typed value
+          onChange={(e) => {
+            setReferredEmail(e.target.value);
+          }}
+        ></input>
+        
         <div class="referral-code-background">Your Referral Code</div>
       </div>
     </div>
