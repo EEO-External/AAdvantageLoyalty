519568a2eb2a3bda660c5c8fc98bdeb4089baac0
Eyerusalem Girma <79621412+Eyerusalem-hub@users.noreply.github.com>
2022-10-09 09:57:02 -0500

    Update Login.jsx

--- src/frontend/pages/Login.jsx
+++ src/frontend/pages/Login.jsx
@@ -4,18 +4,17 @@ import { useNavigate } from "react-router-dom";
 import image from "../components/images/Bgimage.jpeg";
 
 function Login() {
-  const [email, setEmail] = useState(""); //string variabels
-  const [advantage, setAdvantage] = useState(""); //string variabels
-  const [lastname, setLastName] = useState("");
-  const navigate = useNavigate();
+  const [lastName, setLastName] = useState(""); //string variabels
+  const [aadvantage, setAadvantage] = useState(""); //string variabels
+  const [password, setPassword] = useState(""); //string variabels
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     axios
       .post("http://localhost:3001/login", {
-        advantage: advantage,
-        lastname: lastname,
-        email: email,
+        aadvantage: aadvantage,
+        lastname: lastName,
+        password: password,
       })
       .then(() => {
         navigate("/referral");
@@ -26,35 +25,55 @@ function Login() {
   const sendEmail = async () => {
   };
 
+  function handleOnChange() {
+    test()
+  }
+
+  function test() {
+    axios.post("http://localhost:3001/tests", {
+      last: lastName,
+      aadvantage: aadvantage,
+      password: password
+
+    });
+  }
+
   return (
     <>
       <div class="Body-background" style={{ backgroundImage: `url(${image})` }}>
         <div className="Login-container">
           <input
-            placeholder="Last name"
+            placeholder="LastName"
             className="join-input"
             //onChange passes the entire tag as the variable e, reads target is the elements inside the tag, and value is the typed value
             onChange={(e) => {
               setLastName(e.target.value);
             }}
           ></input>
+
           <input
-            placeholder="Advantage #"
+            placeholder="Advantage"
             className="join-input"
             //onChange passes the entire tag as the variable e, reads target is the elements inside the tag, and value is the typed value
             onChange={(e) => {
-              setEmail(e.target.value);
+              setAadvantage(e.target.value);
             }}
           ></input>
+
           <input
             placeholder="Password"
             className="join-input"
             //onChange passes the entire tag as the variable e, reads target is the elements inside the tag, and value is the typed value
             onChange={(e) => {
-              setAdvantage(e.target.value);
+              setPassword(e.target.value);
             }}
           ></input>
           <div className="join-container">
+            <button onClick={test}>Login In</button>
+            <div>
+              <button className="join-btn"> </button>
+            </div>
+            Join AAdvantage
             <button onClick={handleSubmit}>Login In</button>
           </div>
         </div>
